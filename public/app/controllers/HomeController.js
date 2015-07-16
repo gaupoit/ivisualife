77d723ae49d30844ac0e03b85015a02cc385a3a8
Thinh Nguyen <phuoc@niometrics.com>
2015-07-17 00:55:04 +0800

    Validate only after click submit

--- public/app/controllers/HomeController.js
+++ public/app/controllers/HomeController.js
@@ -1,22 +1,20 @@
 lifeApp.controller('HomeController', ['$scope', '$http', '$route', '$location',
     function($scope, $http, $route, $location) {
-
         $scope.appName = 'Your Life in Stats';
-
-        $scope.visualize = function(vm) {
-
-            vm.bod = vm.bod_year + "-" + vm.bod_month + "-" + vm.bod_day;
-
-            $http.post('api/users', vm)
-                .success(function(data) {
+        $scope.vm = {};
+        $scope.visualMyLife = function(isValid) {
+            $scope.submitted = true;
+            if (isValid) {
+                var vm = $scope.vm;
+                vm.bod = vm.bod_year + "-" + vm.bod_month + "-" + vm.bod_day;
+                $http.post('api/users', vm).success(function(data) {
                     console.log(data);
                     $location.path('/life/' + data._id);
-
-                })
-                .error(function(data) {
+                }).error(function(data) {
                     console.log("Errors", data);
                 });
-
+            }
         };
-
-}]);
+        $scope.visualize = $scope.visualMyLife;
+    }
+]);
