0982e356247cd6c959b92bdcef4ffdf971504976
Thinh Nguyen <phuoc@niometrics.com>
2015-11-07 15:24:21 +0800

    Cron job to get world population everyday

--- core/population.go
+++ core/population.go
@@ -5,28 +5,64 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"log"
+    "gopkg.in/mgo.v2/bson"
+    "github.com/gaupoit/lib"
+    "github.com/robfig/cron"
 )
 
 var (
 	populationIoUrl = "http://api.population.io:80/1.0/"
+	dbUri = "mongodb://localhost"
+	db = "ivl"
+	collection = "population"
 )
 
-type population struct {
+type Population struct {
 	Date string
-	Population float64
+	Data float64
 }
 
 func main() {
-	fmt.Println(populationIo)
-	country := "World"
-	year := "today-and-tomorrow"
-	getPopulation(country, year)
+	c := cron.New()
+	c.AddFunc("@daily", func() {
+		country := "World"
+		year := "today-and-tomorrow"
+		result := getPopulation(country, year)
+		for _, value := range result {
+			insert(value)
+		}	
+	})
+	c.Start()
+	select {}
 }
 
-func getPopulation(country string, year string) {
+func insert(population Population) {
+	session := lib.ConnectDb(dbUri)
+	defer session.Close()
+	
+	c := session.DB(db).C(collection)
+	err := c.Insert(population)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Population{}
+	err = c.Find(bson.M{"date": population.Date}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("World Population of %s: %d\n", result.Date, uint(result.Data))
+}
+
+func getPopulation(country string, year string) []Population {
+	result := make([]Population, 0)
 	url := fmt.Sprintf("%s/population/%s/%s", populationIoUrl, country, year)
 	fmt.Printf("Getting data from: %s\n", url)
 	resp, err := http.Get(url)
+
 	if err == nil {
 		body, err := ioutil.ReadAll(resp.Body)
 		var data map[string][]map[string]interface{}
@@ -34,18 +70,19 @@ func getPopulation(country string, year string) {
 		if err == nil {
 			total_population := data["total_population"]
 			for _, v := range total_population {
-				var pop population
+				var pop Population
 				pop.Date = v["date"].(string)
-				pop.Population = v["population"].(float64)
-				fmt.Println(pop)
+				pop.Data = v["population"].(float64)
+				result = append(result, pop)
 			}
 		} else {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
 	} else {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	
+
+	return result
 }
 
 
